c4c60d88c9fb2be779a70ef6f2defc6f56cf6ae7
Juan Carrasquer <juancarrasquer@gmail.com>
2024-10-01 01:06:57 +0200

    inicio del microservicio user + dbusers

---
+++ microservice_users/src/main/java/com/juan/microservice_users/MicroserviceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.juan.microservice_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUsersApplication.class, args);
+	}
+
+}
